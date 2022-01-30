6efdb27bbf4242e92c7d1d96b6295f76b6196966
Lazzy <64617706+JustLazzy@users.noreply.github.com>
2022-01-30 18:25:32 +0900

    Update qb-inventory.sql

--- qb-inventory.sql
+++ qb-inventory.sql
@@ -14,14 +14,6 @@ CREATE TABLE IF NOT EXISTS `stashitems` (
   KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS 'mailboxitems' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'mailbox' varchar(255) DEFAULT NULL,
-  'items' longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY ('mailbox'),
-  KEY 'id' ('id')
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
 CREATE TABLE IF NOT EXISTS `trunkitems` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `plate` varchar(255) DEFAULT NULL,
