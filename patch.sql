f5ee4467c443e415b15449f55420b883e50ad58b
Lazzy <64617706+JustLazzy@users.noreply.github.com>
2022-02-01 09:51:08 +0900

    Update patch.sql

--- patch.sql
+++ patch.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS 'mailboxitems' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'mailbox' varchar(255) DEFAULT NULL,
-  'items' longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY ('mailbox'),
-  KEY 'id' ('id')
+CREATE TABLE IF NOT EXISTS `mailboxitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mailbox` varchar(255) DEFAULT NULL,
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`mailbox`),
+  KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
